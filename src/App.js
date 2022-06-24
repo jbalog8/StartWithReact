bb2a6269606973e4126534fb1711ff28fd065c52
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-06-24 21:18:27 +0200

    Updatig code im my frist project

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
-import './App.css';
-
-function InputElement({ lable, type }) {
-  return (
-    <label>
-      <div className="lable">{lable}</div>
-      <input className="input" type={type} />
-    </label>
-  );
-}
+import "./App.css";
+import { InputElement } from "./components/InputElement";
+import Button from "./components/Button";
 
 function App() {
   return (
     <form className="form">
       <div className="form-field">
-        <InputElement lable="Username" type="text" />
+        <InputElement label="Username" type="text" />
       </div>
       <div className="form-field">
-        <InputElement lable="Password" type="password" />
-
+        <InputElement label="Password" type="password" />
       </div>
       <div className="form-field">
-        <button className="button">Sign in</button>
+        <Button />
       </div>
     </form>
-    
-  )
-
-
+  );
 }
 
 export default App;
