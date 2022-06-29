b04f8421dd0ee9ab70fa1f5a13532616a995f8d1
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-06-29 20:17:35 +0200

    Adding events

--- src/App.js
+++ src/App.js
@@ -4,35 +4,44 @@ import Button from "./components/Button";
 import FormError from "./components/FormError";
 import FormMessage from "./components/FormMessage";
 import { User } from "./components/User";
-import {Counter} from "./components/Counter";
+import { Counter } from "./components/Counter";
 
 function App() {
   const isSignedIn = true;
- 
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('submit');
+  }
+
+  const handelChange = (evet) => {
+    console.log(evet);
+  }
+
   return (
     <div>
-        {isSignedIn && <User src="/EricCartman.png" alt="Eric" username="Eric"/>}
-      
-      <form className="form">
+      {isSignedIn && <User src="/EricCartman.png" alt="Eric" username="Eric" />}
+
+      <form className="form" onSubmit={handleSubmit}>
         <div className="form-field">
-          <InputElement label="Username" type="text" />
+          <InputElement label="Username" type="text" onChange={handelChange} />
         </div>
-        
-      <div className="form-field">
-        <InputElement label="Password" type="password" />
+
+        <div className="form-field">
+          <InputElement label="Password" type="password" onChange={handelChange} />
         </div>
-        
-      <div className="form-field">
-        <Button type="button">Sign in</Button>
-        <Button type="reset">Reset</Button>
-      </div>
-        
-      <FormError visible={!isSignedIn} />
-      <FormMessage visible={isSignedIn} />
-     </form>
+
+        <div className="form-field">
+          <Button type="submit">Sign in</Button>
+          <Button type="reset">Reset</Button>
+        </div>
+
+        <FormError visible={!isSignedIn} />
+        <FormMessage visible={isSignedIn} />
+      </form>
       <Counter initialValue={0} />
     </div>
-     
+
   );
 }
 
