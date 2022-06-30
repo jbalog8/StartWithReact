ce0ab9b3dd9811c1940426bf275a13061e7bcec6
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-06-30 18:16:54 +0200

    Remove password

--- src/App.js
+++ src/App.js
@@ -4,23 +4,23 @@ import Button from "./components/Button";
 import FormError from "./components/FormError";
 import FormMessage from "./components/FormMessage";
 import { User } from "./components/User";
-import { Counter } from "./components/Counter";
+
 import { useState } from "react";
 
 function App() {
 
-  const [formState, setFormState] = useState({ username: '', password: '' });
+  const [formState, setFormState] = useState({ username: '' });
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    if (formState.password === 'password') {
-      setUser(formState);
-      setError(null);
+    if (formState.username === '') { setError(true);
+      
     } else {
-      setError(true);
+     setUser(formState);
+      setError(null);
     }
   }
 
@@ -50,7 +50,7 @@ function App() {
           <InputElement name="username" label="Username" type="text" onChange={handleChange} />
         </div>
         <div className="form-field">
-          <InputElement name="password" label="Password" type="password" onChange={handleChange} />
+          
         </div>
         <div className="form-field">
           <Button type="submit">Sign in</Button>
@@ -62,7 +62,7 @@ function App() {
       </form>}
 
       
-      <Counter initialValue={0} />
+    
 
        {isSignedIn && <Button type="button" onClick={signOut}>Sign out</Button>}
 
