40cfb34f2ac252ce891b8a5acc34405150ede5e1
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-07-08 19:09:21 +0200

    Add mock API for NASA photo of the day

--- src/components/NasaPhotoOfTheDay/NasaPhotoOfTheDay.jsx
+++ src/components/NasaPhotoOfTheDay/NasaPhotoOfTheDay.jsx
@@ -1,7 +1,47 @@
 import "./NasaPhotoOfTheDay.css";
 import data from "./data.json";
+import { useState } from "react";
+import { useEffect } from "react";
+
+function getPhotoOfTheDay() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      
+      resolve(data);
+    }, 5000);
+  });
+}
 
 export function NasaPhotoOfTheDay() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getPhotoOfTheDay()
+      .then((data) => {
+        setData(data)
+      })
+      .catch(() => {
+        setError(new Error("Something went wrong"));
+      
+      });
+  }, []);
+
+  console.log(data);
+
+  if (error !== null) {
+    return <div>
+      Something went wrong
+    </div>
+  }
+ 
+  
+  if (data === null) {
+    return <div>
+      Loading...
+    </div>
+  }
+
   return (
     <figure className="nasa-photo-of-the-day">
       <img src={data.url} alt={data.title} />
